3789da49f6250fd89c20355b67605050fecb1c61
Adrien <157978359+SONGOKU92@users.noreply.github.com>
2024-11-07 18:03:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ typewriter
 .typeString('<strong>, Etudiant en BTS SIO </strong> !')
 .pauseFor(2300)
 .deleteChars(23)
-.typeString('<span style="color: #bb0b0b;"> Software Architect, </span> !')
+.typeString('<span style="color: #191970;"> Software Architect, </span> !')
 .start()
 
 
